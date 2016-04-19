6fe9ecb218b8f1271c5123c2bb0c782ea9220528
Guo-Tai Huang <john7714060@hotmail.com.tw>
2016-04-19 21:11:34 +0800

    DB AUTH and REG Feature

---
+++ Share-Books/src/main/webapp/WEB-INF/classes/DB_API.java
@@ -0,0 +1,131 @@
+import java.sql.*;
+
+interface DB_API {
+	public String[] Auth(String email, String passwd);
+
+	public Boolean Reg(String[] info);
+}
+
+class DB implements DB_API {
+	private final String DB_URL = "jdbc:mysql://140.134.26.83/sharebooks";
+
+	// Database credentials
+	private final String USER = "sharebooks";
+	private final String PASS = "fcuspm";
+
+	public String[] Auth(String email, String passwd) {
+		String[] result = new String[5];
+		Connection conn = null;
+		Statement stmt = null;
+
+		try {
+			// STEP 2: Register JDBC driver
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// STEP 3: Open a connection
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+			// STEP 4: Execute a query
+			System.out.println("Creating statement...");
+			stmt = conn.createStatement();
+
+			String sql = "SELECT * FROM sharebooks.user_data where email='" + email + "' and pw='" + passwd + "'";
+			ResultSet rs = stmt.executeQuery(sql);
+
+			// STEP 5: Extract data from result set
+
+			if (rs.next()) {
+				// Retrieve by column name
+				result[0] = Integer.toString(rs.getInt("id"));
+				result[1] = rs.getString("username");
+				result[2] = rs.getString("email");
+				result[3] = rs.getString("pw");
+				result[4] = Integer.toString(rs.getInt("phone"));
+			}
+			// STEP 6: Clean-up environment
+			rs.close();
+
+			stmt.close();
+			conn.close();
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			e.printStackTrace();
+		} finally {
+			// finally block used to close resources
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException se2) {
+			} // nothing we can do
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			} // end finally try
+		} // end try
+		return result;
+	}
+
+	public Boolean Reg(String info[]) { //info{username,email,pw,phone}
+		if (info.length != 4) {
+			return false;
+		}
+		Connection conn = null;
+		Statement stmt = null;
+
+		try {
+			// STEP 2: Register JDBC driver
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// STEP 3: Open a connection
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+			// STEP 4: Execute a query
+			System.out.println("Creating statement...");
+			stmt = conn.createStatement();
+
+			String sql = "SELECT * FROM sharebooks.user_data where username='" + info[0] + "' and email='" + info[1]
+					+ "'";
+			ResultSet rs = stmt.executeQuery(sql);
+
+			if (rs.next()) {
+				return false;
+			}
+			rs.close();
+
+			sql = "insert into sharebooks.user_data(username,email,pw,phone) values('" + info[0] + "','" + info[1]
+					+ "','" + info[2] + "','" + info[3] + "')";
+			stmt.executeUpdate(sql);
+
+			stmt.close();
+			conn.close();
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			e.printStackTrace();
+		} finally {
+			// finally block used to close resources
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException se2) {
+			} // nothing we can do
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			} // end finally try
+		} // end try
+
+		return true;
+	}
+}
