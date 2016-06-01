08070f372e489031ed012782d52b2f5b58e50e70
XinHung <wishes10707@gmail.com>
2016-06-01 15:04:17 +0800

    1. DB.java增加抓取所有書籍資訊的method 2. 更新搜尋結果頁面(待完成)

--- Share-Books/src/main/java/dbapi/DB.java
+++ Share-Books/src/main/java/dbapi/DB.java
@@ -596,4 +596,63 @@ public class DB {
 
 		return "更新成功";
 	}
+	
+	public String[][] getBookInfo() {
+		Connection conn = null;
+		Statement stmt = null;
+		String[][] result = null;
+
+		try {
+			// STEP 2: Register JDBC driver
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// STEP 3: Open a connection
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+			// STEP 4: Execute a query
+			System.out.println("Creating statement...");
+			stmt = conn.createStatement();
+
+			String sql = "SELECT * FROM sharebooks.book_data";
+			ResultSet rs = stmt.executeQuery(sql);
+			result = new String[6][5];
+			// STEP 5: Extract data from result set
+
+			int i=0;
+			while (rs.next()) {
+
+				result[i][0] = new String(rs.getString("bookname").getBytes("utf-8"), "utf-8");
+				result[i][1] = new String(rs.getString("description").getBytes("utf-8"), "utf-8");
+				result[i][2] = new String(rs.getString("author").getBytes("utf-8"), "utf-8");
+				result[i][3] = new String(rs.getString("publisher").getBytes("utf-8"), "utf-8");
+				result[i][4] = new String(rs.getString("publish_date").getBytes("utf-8"), "utf-8");
+				i++;
+
+			} 
+			// STEP 6: Clean-up environment
+			rs.close();
+			conn.close();
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			e.printStackTrace();
+		} finally {
+			// finally block used to close resources
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException se2) {
+			} // nothing we can do
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			} // end finally try
+		} // end try
+		return result;
+	}
 }
